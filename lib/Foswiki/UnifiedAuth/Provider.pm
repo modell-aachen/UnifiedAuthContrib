aba122adf8bfe719bfa4647977eea734b1ddf278
Sven Meyer <meyer@modell-aachen.de>
2016-12-20 08:37:11 +0100

    Added simple caching for a provider id
    Ticket: none
    Release-Note: none

--- lib/Foswiki/UnifiedAuth/Provider.pm
+++ lib/Foswiki/UnifiedAuth/Provider.pm
@@ -72,11 +72,15 @@ sub supportsRegistration {
 sub getPid {
     my ( $this ) = @_;
 
+    return $this->{internal_provider_id} if defined $this->{internal_provider_id};
     my $uauth = Foswiki::UnifiedAuth->new();
     my $db = $uauth->db;
     my $pid = $db->selectrow_array("SELECT pid FROM providers WHERE name=?", {}, $this->{id});
 
-    return $pid if($pid);
+    if ($pid) {
+        $this->{internal_provider_id} = $pid;
+        return $pid;
+    }
 
     Foswiki::Func::writeWarning("Could not get pid of $this->{id}; creating a new one...");
     $db->do("INSERT INTO providers (name) VALUES(?)", {}, $this->{id});
